4864d63ad9b23322f79c4ad657cef8d311d9ac85
BloodReaper <67401865+BigDeeLoki@users.noreply.github.com>
2022-06-20 19:52:37 +0000

    added a lot

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+list_size = [700,600]
+size = (700,600)
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+
+def main():
+  print("Starting game...")
+
+
+
+
+if __name__ == "__name__":
+  main()
